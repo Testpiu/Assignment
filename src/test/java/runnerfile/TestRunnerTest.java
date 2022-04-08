74a358343be5b8dec69b302c531e53d742171830
Testpiu <mohitepriya304@gmail.com>
2022-04-09 00:23:20 +0530

    adding the project firstly

---
+++ src/test/java/runnerfile/TestRunnerTest.java
@@ -0,0 +1,27 @@
+package runnerfile;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+	
+	@RunWith(Cucumber.class)
+	@CucumberOptions(
+	        features="classpath:features",
+	        glue="Assignment.stepdefs", 
+	        tags="", 
+	        plugin = {"pretty",
+	            "html:target/html/htmlreport.html",
+	            "json:target/json/file.json",
+	            },
+	        monochrome=true,
+	        publish=true,
+	        dryRun=false
+	        )
+
+	public class TestRunnerTest {
+
+	}
+
+
